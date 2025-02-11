460b18d28fcdfd49142986dda9caa61f29152d4d
Subhash Yogi <subhashyogi999@gmail.com>
2025-02-11 16:17:52 +0530

    updated

--- Backend/index.js
+++ Backend/index.js
@@ -6,17 +6,22 @@ const QuestionRouters = require("./Routers/QuestionRouters");
 // require("dotenv").config();
 
 // const OPENAI_API_KEY = process.env.API_KEY;
+const DBURL =
+  "mongodb+srv://Monkey_D_Luffy:(@S1u2b3h4a5s6h7@)@quizzapp.laibk.mongodb.net/QuizzApp?retryWrites=true&w=majority&appName=quizzapp";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 app.use("/User", UserRouter);
-app.use('/Question', QuestionRouters);
+app.use("/Question", QuestionRouters);
 
 mongoose
-  .connect("mongodb://localhost:27017/", {
-    dbName: "Users",
+  .connect(DBURL, {
+    // useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useFindAndModify: false,
   })
   .then((result) => {
     console.log("Connected to MongoDB");
@@ -24,6 +29,6 @@ mongoose
       console.log("server started");
     });
   })
-  .catch((error) => { 
+  .catch((error) => {
     console.log(error.message);
   });
